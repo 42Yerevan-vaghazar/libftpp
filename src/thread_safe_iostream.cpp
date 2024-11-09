495a660061f36651fe29f502b4b2ac020ebf9ad4
vahanafk <vahan.ghazaryan.00@mail.ru>
2024-11-09 21:50:04 +0400

    some work on thread safe iostream

--- src/thread_safe_iostream.cpp
+++ src/thread_safe_iostream.cpp
@@ -1,2 +1,11 @@
 #include "thread_safe_iostream.hpp"
 
+thread_local ThreadSafeIOStream threadSafeCout;
+// thread_local std::ostringstream ThreadSafeIOStream::_buffer;
+// thread_local std::string ThreadSafeIOStream::threadLocalPrefix;
+
+void ThreadSafeIOStream::setPrefix(const std::string& prefix) {
+    const std::lock_guard<std::mutex> lock(_mutex);
+    threadLocalPrefix = prefix;
+}
+
--- src/thread_safe_iostream.hpp
+++ src/thread_safe_iostream.hpp
@@ -3,55 +3,33 @@
 #include <unordered_map>
 #include <thread>
 #include <mutex>
+#include <sstream>
+
 
 class ThreadSafeIOStream
 {
 public:
-    ThreadSafeIOStream() : _isPrefixPrinted(false){}
-    template <typename T>
-    friend ThreadSafeIOStream &operator<<(ThreadSafeIOStream &obj, const T &str) ;
-    friend inline ThreadSafeIOStream &operator<<(ThreadSafeIOStream &obj, std::ostream& (*manip)(std::ostream&));  
-    void setPrefix(const std::string& prefix) {
-        const std::lock_guard<std::mutex> lock(_scopeMutex);
-        _prefixs[std::this_thread::get_id()] = {prefix, false};
-    }
-private:
-    struct thread_info
-    {
-        std::string prefix;
-        bool isPrefixPrinted;
-    };
+    void setPrefix(const std::string& prefix);
 
-    std::unordered_map<std::thread::id, thread_info> _prefixs;
-    bool _isPrefixPrinted;
-    std::mutex _ownershipMutex;
-    std::mutex _scopeMutex;
+    template <typename T>
+    ThreadSafeIOStream &operator<<(const T &str) {
+        _buffer << str;
+        return (*this);
+    }
+
+    ThreadSafeIOStream &operator<<(std::ostream& (*manip)(std::ostream&)) {
+    const std::lock_guard<std::mutex> lock(_mutex);
+
+    std::cout << threadLocalPrefix << _buffer.str() << manip << std::flush;
+    _buffer.str("");
+    _buffer.clear();
+    return (*this);
+}
+private:
+
+    std::ostringstream _buffer;
+    std::string threadLocalPrefix;
+    std::mutex _mutex;
 };
 
-template <typename T>
-ThreadSafeIOStream &operator<<(ThreadSafeIOStream &obj, const T &str) {
-    const std::lock_guard<std::mutex> lock(obj._scopeMutex);
-    auto &[prefix, _isPrefixPrinted] = obj._prefixs[std::this_thread::get_id()];
-
-    if (_isPrefixPrinted == false) {
-        obj._ownershipMutex.lock();
-        _isPrefixPrinted = true;
-        std::cout << prefix;
-    }
-    std::cout << str;
-    return (obj);
-}
-             
-
-inline ThreadSafeIOStream &operator<<(ThreadSafeIOStream &obj, std::ostream& (*manip)(std::ostream&)) {
-    const std::lock_guard<std::mutex> lock(obj._scopeMutex);
-    auto &[prefix, _isPrefixPrinted] = obj._prefixs[std::this_thread::get_id()];
-    if (_isPrefixPrinted == false) {
-        _isPrefixPrinted = true;
-        std::cout << prefix;
-    }
-    std::cout << manip;
-    _isPrefixPrinted = false;
-    obj._ownershipMutex.unlock();
-    return (obj);
-}
+extern thread_local ThreadSafeIOStream threadSafeCout; 
